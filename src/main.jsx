092da08277a4431c3c3f141c073072d4ede0b82f
Johan Camilo Delgado Gomez <kmilogomez154@gmail.com>
2022-11-27 14:45:59 -0500

    [main] clean workspace

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react'
+
+import "normalize.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <p>Hello world!</p>
   </React.StrictMode>
-)
+);
